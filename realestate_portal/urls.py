2a8ec4277ce0ec82ec365d9fb965f6a6c20b7c51
ktourek3 <165823659+ktourek3@users.noreply.github.com>
2025-11-15 14:32:20 -0600

    Adding things for pythonanywhere push

--- realestate_portal/urls.py
+++ realestate_portal/urls.py
@@ -19,10 +19,14 @@ Including another URLconf
 URL configuration for realestate_portal project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('listings.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 ]
